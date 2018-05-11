638050468ca2040f79969cbda85fa319634f6ef8
auziabbruzzesi <austinta@udel.edu>
2018-05-11 13:57:36 -0400

    blah

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+
 #define STRING_SIZE 1024
 
 typedef struct{
@@ -25,10 +27,64 @@ typedef struct{
 
 //Banker's algorithm
 
-void parse(File * file){
-  //while file newline stuff
+void config(int start_time, int M, int S, int Q){
+  //TODO
+}
 
-  //if config line:
+void parse(char * line){
+ // printf("parsing line: %s\n", line);
+  
+  char * cmd_type;
+  const char config = 'C';
+  const char job_arrival = 'A';
+  const char device_request = 'Q';
+  const char device_release = 'L';
+  const char system_status = 'D';
+  int start_time;
+  char * params;
+  char * vals;
+  //printf("first char: %c\n",line[0]);
+  //while file newline stuff
+  
+  int M;
+  int S;
+  int Q;
+  int J; 
+  int R;
+  int P;
+  switch(line[0]){
+    case 'C':
+      
+      printf("this is a config line!\n");
+      params = strtok(line,"C ");
+      //printf("params: %s\n", params);
+      printf("%s\n",params);
+      
+      printf("parameter line: %s\n",params);
+      start_time = atoi(params);
+      params = strtok(params,"C M= S= Q= ");
+      M = atoi(params);
+      params = strtok(params,"C M= S= Q= ");
+      S = atoi(params);
+      params = strtok(params,"C M= S= Q= ");
+      Q = atoi(params);
+      params = strtok(params,"C M= S= Q= ");
+      printf("Start time = %d, M = %d, S = %d, Q = %d\n", start_time, M, S, Q);
+
+      
+      
+      
+    case 'A':
+      printf("this is a job arrival\n");
+    case 'Q':
+      printf("this is a device request\n");
+    case 'L':
+      printf("This is a device release\n");
+    case 'D':
+      printf("this is a system status\n");
+  }
+
+  
     //config(params);
   //if job line:
     //new_job(params)
@@ -43,17 +99,32 @@ void parse(File * file){
 
 int main(int argc, char ** argv){
   char filename[STRING_SIZE];
+  printf("enter config. file name: \n");
+  scanf("%s", filename);
   FILE *file;
   file = fopen(filename, "r");
   size_t buffer = STRING_SIZE;
   char * line = (char*)malloc(STRING_SIZE*sizeof(char));
-  
-  
-  getline(&line,&buffer,file);
-  printf("enter config. file name: \n");
-  scanf("%s", filename);
+  if(file){
+    while(getline(&line,&buffer,file)>=0){
+      parse(line);
 
-  printf("%s",line);
+    }
+
+
+
+
+
+
+
+  }
+  
+  
+  
+  
+  
+
+  
 
   //TODO write parse function to parse the text file
 
